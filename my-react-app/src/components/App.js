4a575f89efe3653fd33d20734e1b4cfecdecf97e
Dayeon Hwang <dahwan@microsoft.com>
2025-01-01 02:28:33 -0800

    initial react app

---
+++ my-react-app/src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../App.css';
+
+const App = () => {
+    return (
+        <div>
+            <div className="cloud" id="cloud1"></div>
+            <div className="cloud" id="cloud2"></div>
+            <h1>Hi, I'm Dayeon Hwang</h1>
+        </div>
+    );
+};
+
+export default App;
