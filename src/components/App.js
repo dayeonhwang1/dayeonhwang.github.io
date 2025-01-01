671bf795e2f039d6fe7a7ea230849105aa595160
Dayeon Hwang <dahwan@microsoft.com>
2025-01-01 06:06:00 -0800

    make button animation better

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import '../App.css';
 
 const App = () => {
-    const [showButton, setShowButton] = useState(false);
-
-    useEffect(() => {
-        const timer = setTimeout(() => {
-            setShowButton(true);
-        }, 3500); // Match the duration of the typing animation
-
-        return () => clearTimeout(timer);
-    }, []);
-
     return (
         <div className="title-container">
             <div className="title">Hi, I'm Dayeon Hwang.</div>
-            {showButton && <button className="coming-soon-button">Coming Soon</button>}
+            <button className="coming-soon-button">Coming Soon</button>
         </div>
     );
 };
